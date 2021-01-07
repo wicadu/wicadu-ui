16655ebd0693256b9f5e864d5ee84dcdfc96ce6e
Joaquin Araujo <hola@joaquinaraujojs.com>
2021-01-07 11:17:31 -0300

    add form element

--- src/hocs/Form.tsx
+++ src/hocs/Form.tsx
@@ -1,22 +1,30 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { useForm, useWatch, FormProvider, useFormContext } from 'react-hook-form'
 import PropTypes, { InferProps } from 'prop-types'
 
 const propTypes = {
   children: PropTypes.node.isRequired,
-  opts: PropTypes.object
+  opts: PropTypes.object,
+  onSubmit: PropTypes.func.isRequired
 }
 
 type Props = InferProps<typeof propTypes>
 
 const defaultProps: Props = {
-  opts: {}
+  opts: {},
+  onSubmit () {}
 }
 
-function Form ({ children, opts }: Props) {
+function Form ({ children, opts, onSubmit }: Props) {
   const methods = useForm<any>(opts)
 
-  return <FormProvider {...methods}>{children}</FormProvider>
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={methods.handleSubmit(onSubmit)}>
+        {children}
+      </form>
+    </FormProvider>
+  )
 }
 
 Form.propTypes = propTypes
