1a0cf200408aa377271fc051442901705365a600
Jonathan Araujo <j4asdev@gmail.com>
2021-02-24 11:57:00 -0300

    export theme hoc

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import Form from './hocs/Form'
+import Theme from './hocs/Theme'
 import Button from './atoms/Button'
 import Input from './atoms/Input'
 import Spin from './atoms/Spin'
@@ -6,6 +7,7 @@ import Typography from './atoms/Typography'
 
 export {
   Form,
+  Theme,
   Button,
   Input,
   Spin,
